e2755f649bfcfde71e25ee21afd4aaf9141a88e9
Lucas Martins <lucassena@alunos.utfpr.edu.br>
2024-04-24 07:21:03 -0300

    update movie entity
    attributes and relations for movie entity

--- src/movie/entities/movie.entity.ts
+++ src/movie/entities/movie.entity.ts
@@ -1,3 +1,63 @@
-import { BaseEntity } from 'src/entities/Base.entity';
+import { Entity, Column, ManyToMany } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsOptional,
+  IsNumber,
+  IsDate,
+  IsArray,
+} from 'class-validator';
+import { Genre } from '../../genre/entities/genre.entity';
+import { Director } from '../../director/entities/director.entity';
+import { BaseEntity } from '../../entities/base.entity';
 
-export class Movie extends BaseEntity {}
+@Entity()
+export class Movie extends BaseEntity {
+  constructor() {
+    super();
+  }
+
+  @IsString()
+  @Column()
+  @ApiProperty({ required: true, example: 'The Shawshank Redemption' })
+  title: string;
+
+  @IsString()
+  @Column('text')
+  @ApiProperty({
+    required: true,
+    example: 'Two imprisoned men bond over a number of years...',
+  })
+  description: string;
+
+  @IsDate()
+  @Column('date')
+  @ApiProperty({ required: true, example: '1994-09-22' })
+  releaseDate: Date;
+
+  @IsNumber()
+  @Column('int')
+  @ApiProperty({ required: true, example: 142 })
+  duration: number;
+
+  @IsNumber()
+  @Column('decimal', { precision: 5, scale: 2 })
+  @ApiProperty({ required: true, example: 9.3 })
+  rating: number;
+
+  @IsOptional()
+  @IsString()
+  @Column({ nullable: true })
+  @ApiProperty({ required: false })
+  coverImageUrl?: string;
+
+  @IsArray()
+  @ManyToMany(() => Genre)
+  @ApiProperty({ required: false, type: () => [Genre] })
+  genres: Genre[];
+
+  @IsArray()
+  @ManyToMany(() => Director)
+  @ApiProperty({ required: false, type: () => [Director] })
+  directors: Director[];
+}
