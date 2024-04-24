59322e9a60e889dea422b08a85933f84cdb6f97c
Lucas Martins <lucassena@alunos.utfpr.edu.br>
2024-04-24 06:00:59 -0300

    project structure and swagger config

---
+++ src/libs/swagger.spec.ts
@@ -0,0 +1,52 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigSwagger } from './swagger';
+
+const mockSwaggerModuleCreateDocument = jest.fn();
+const mockSwaggerModuleSetup = jest.fn();
+
+const mockDocumentBuilder = {
+  setTitle: jest.fn(() => ({
+    setDescription: jest.fn(() => ({
+      setVersion: jest.fn(() => ({
+        addBearerAuth: jest.fn(() => ({
+          build: jest.fn(),
+        })),
+      })),
+    })),
+  })),
+};
+
+jest.mock('@nestjs/swagger', () => {
+  return {
+    __esModule: true,
+    ...{
+      SwaggerModule: {
+        createDocument: jest.fn(() => mockSwaggerModuleCreateDocument()),
+        setup: jest.fn(() => mockSwaggerModuleSetup()),
+      },
+      DocumentBuilder: jest.fn().mockImplementation(() => mockDocumentBuilder),
+    },
+  };
+});
+
+describe('Swagger config class', () => {
+  let configSwagger: ConfigSwagger;
+
+  beforeEach(() => {
+    configSwagger = new ConfigSwagger({} as unknown as INestApplication);
+  });
+
+  it('should have a create document method', () => {
+    expect(configSwagger.createDocument).toBeDefined();
+  });
+
+  it('should call createDocument method from Swagger module when createDocument method is called', () => {
+    configSwagger.createDocument();
+    expect(mockSwaggerModuleCreateDocument).toHaveBeenCalled();
+  });
+
+  it('should call setup method from Swagger module when createDocument method is called', () => {
+    configSwagger.createDocument();
+    expect(mockSwaggerModuleSetup).toHaveBeenCalled();
+  });
+});
